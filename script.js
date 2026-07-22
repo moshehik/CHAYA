544643b42559ed1f92de708704328ad379c1398e
Moshe <moshe@example.com>
2026-07-22 14:10:24 +0300

    Fix CORS issue by sending fetch request as text/plain

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ async function sendEmail({ subject, body, file }) {
         const response = await fetch(scriptUrl, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'text/plain;charset=utf-8'
             },
             body: JSON.stringify(googlePayload)
         });
